0d316980cbe92c295a9f0c898c5c5e13209d4aa1
Bruno Oliveira <31875213+Bruno-Oliveira-e-Oliveira@users.noreply.github.com>
2025-02-13 10:21:18 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Olá mãe se você está lendo isso dê um sorriso :)</h1>
+  return <h1>Olá mãe se você está lendo isso dê um sorriso :)</h1>;
 }
 
 export default Home;
